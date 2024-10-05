0a98d3dbd2608f76197714696bfa1ec61b0d89d7
Gawevi <gbielwade@gmail.com>
2024-10-05 15:12:03 -0300

    adição das páginas de produto

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,11 +1,14 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 import { Link } from 'react-router-dom'
+import { Props } from '.'
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<Props>`
   color: ${cores.branco};
-  background-color: transparent;
-  border: 2px solid ${cores.branco};
+  background-color: ${(props) =>
+    props.variant === 'primary' ? cores.verde : 'transparent'};
+  border: 2px solid
+    ${(props) => (props.variant === 'primary' ? cores.verde : cores.branco)};
   font-size: 16px;
   font-weight: bold;
   padding: 8px 16px;
